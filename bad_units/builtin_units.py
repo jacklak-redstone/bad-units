fb6540c6d91cdecff905131756956021748dd8be
IceWizard <mrrioes.dc@gmail.com>
2025-11-24 21:21:04 +0100

    Started rewrite of bad_units

--- bad_units/builtin_units.py
+++ bad_units/builtin_units.py
@@ -1,51 +1,65 @@
-from .module import Unit
+from .module import Unit, BaseUnitType, UnitType
+
+
+# Base Unit Types
+
+class Length(BaseUnitType):
+    category: str = 'length'
+
+
+class Time(BaseUnitType):
+    category: str = 'time'
+
+
+class Mass(BaseUnitType):
+    category: str = 'mass'
 
 
 class Meter(Unit):
-    unit_type: str = "length"
-    base_units_per: float = 1.0
+    unit_type: UnitType = UnitType(1.0)
+    base_unit_type: BaseUnitType = BaseUnitType(Length.category)
 
 
 class Kilometer(Unit):
-    unit_type: str = "length"
-    base_units_per: float = 1000.0
+    unit_type: UnitType = UnitType(1000.0)
+    base_unit_type: BaseUnitType = BaseUnitType(Length.category)
 
 
 class Centimeter(Unit):
-    unit_type: str = "length"
-    base_units_per: float = 0.01
+    unit_type: UnitType = UnitType(0.01)
+    base_unit_type: BaseUnitType = BaseUnitType(Length.category)
 
 
 class Inch(Unit):
-    unit_type: str = "length"
-    base_units_per: float = 0.0254
+    unit_type: UnitType = UnitType(0.0254)
+    base_unit_type: BaseUnitType = BaseUnitType(Length.category)
 
 
 class Foot(Unit):
-    unit_type: str = "length"
-    base_units_per: float = 0.3048
+    unit_type: UnitType = UnitType(0.3048)
+    base_unit_type: BaseUnitType = BaseUnitType(Length.category)
 
 
 class Kilogram(Unit):
-    unit_type: str = "mass"
-    base_units_per: float = 1.0
+    unit_type: UnitType = UnitType(1.0)
+    base_unit_type: BaseUnitType = BaseUnitType(Mass.category)
 
 
 class Gram(Unit):
-    unit_type: str = "mass"
-    base_units_per: float = 0.001
+    unit_type: UnitType = UnitType(0.001)
+    base_unit_type: BaseUnitType = BaseUnitType(Mass.category)
 
 
 class Pound(Unit):
-    unit_type: str = "mass"
-    base_units_per: float = 0.45359237
+    unit_type: UnitType = UnitType(0.45359237)
+    base_unit_type: BaseUnitType = BaseUnitType(Mass.category)
 
 
 class Second(Unit):
-    unit_type: str = "time"
-    base_units_per: float = 1.0
+    unit_type: UnitType = UnitType(1.0)
+    base_unit_type: BaseUnitType = BaseUnitType(Time.category)
 
 
 class Minute(Unit):
-    unit_type: str = "time"
-    base_units_per: float = 60.0
+    unit_type: UnitType = UnitType(60.0)
+    base_unit_type: BaseUnitType = BaseUnitType(Time.category)
--- bad_units/module.py
+++ bad_units/module.py
@@ -1,176 +1,25 @@
 import typing
 
 
-class UnitError(Exception):
-    def __init__(self, message: str) -> None:
-        self.message: str = message
-        super().__init__(self.message)
+class UnitType:
+    def __init__(self, factor: typing.Union[int, float]) -> None:
+        self.factor: float = factor
+
+
+class BaseUnitType:
+    def __init__(self, category: str) -> None:
+        self.category: str = category
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, BaseUnitType):
+            return NotImplemented
+        return self.category == other.category
 
 
 class Unit:
-    unit_type: str = ''
-    base_units_per: float = 0.0
+    unit_type: UnitType
+    base_unit_type: BaseUnitType  # Length, Mass, Time, etc.
 
-    def __init__(self, amount: typing.Union[int, float] = 1):
-        if amount < 0:
-            raise UnitError("Unit amounts cannot be negative")
-        self.amount = amount
-
-    def to(self: Unit, other: Unit) -> Unit:
-        if not isinstance(other, Unit):
-            raise TypeError("Can only convert to another Unit")
-        if self.unit_type != other.unit_type:
-            raise UnitError("Units must be of the same type")
-        return other.__class__((self.amount * self.base_units_per) / other.base_units_per)
-
-    def __add__(self: Unit, other: typing.Union[Unit, CompoundUnit]) -> typing.Union[Unit, CompoundUnit]:
-        if isinstance(other, Unit):
-            if self.unit_type != other.unit_type:
-                raise UnitError("Units must be of the same type")
-            return self.__class__(
-                (self.base_units_per * self.amount + other.base_units_per * other.amount) / self.base_units_per
-            )
-        elif isinstance(other, CompoundUnit):
-            return other + self
-        else:
-            raise TypeError("Can only add Unit or CompoundUnit")
-
-    def __sub__(self: Unit, other: typing.Union[Unit, CompoundUnit]) -> typing.Union[Unit, CompoundUnit]:
-        if isinstance(other, Unit):
-            if self.unit_type != other.unit_type:
-                raise UnitError("Units must be of the same type")
-            return self.__class__(
-                (self.base_units_per * self.amount - other.base_units_per * other.amount) / self.base_units_per
-            )
-        elif isinstance(other, CompoundUnit):
-            return CompoundUnit(self) - other
-        else:
-            raise TypeError("Can only subtract Unit or CompoundUnit")
-
-    def __truediv__(self: Unit, other: Unit) -> CompoundUnit:
-        return CompoundUnit(self, other)
-
-    def __mul__(self: Unit, other: object) -> typing.Union[Unit, CompoundUnit]:
-        if isinstance(other, float) or isinstance(other, int):
-            return Unit(self.amount * other)
-        elif not isinstance(other, Unit) and not isinstance(other, CompoundUnit):
-            raise TypeError("Can multiply subtract Unit or CompoundUnit")
-        elif isinstance(other, Unit):
-            # TODO: This is just wrong.
-            # Two plain units -> numerator contains both, denominator is None
-            return CompoundUnit(self, other)
-        elif isinstance(other, CompoundUnit):
-            return CompoundUnit(
-                numerator=self * other,
-                denominator=other.denominator
-            )
-        else:
-            raise TypeError("Can only multiply by Unit or CompoundUnit")
-
-    def __repr__(self: Unit) -> str:
-        return f"{self.amount:g} {self.__class__.__name__}"
-
-    def __eq__(self: Unit, other: object) -> bool:
-        if not isinstance(other, Unit):
-            raise UnitError("Units must be of the same type")
-        return (
-            self.unit_type == other.unit_type
-            and self.amount * self.base_units_per == other.amount * other.base_units_per
-        )
-
-
-class CompoundUnit:
-    unit_type: str = ''
-
-    def __init__(
-            self,
-            numerator: typing.Union["Unit", "CompoundUnit"],
-            denominator: typing.Union["Unit", "CompoundUnit", None] = None
-    ) -> None:
-        self.numerator: typing.Union["Unit", "CompoundUnit"] = numerator
-        self.denominator: typing.Union["Unit", "CompoundUnit", None] = denominator
-        self.unit_type = f'{numerator.unit_type}/{denominator.unit_type}'
-
-    def _check_compatibility(self: CompoundUnit, other: CompoundUnit) -> None:
-        if not isinstance(other, CompoundUnit):
-            raise TypeError("Can only add/subtract with another CompoundUnit")
-        if self.numerator.unit_type != other.numerator.unit_type:
-            raise UnitError("Cannot add/subtract: numerators mismatch")
-        if self.denominator and other.denominator:
-            if self.denominator.unit_type != other.denominator.unit_type:
-                raise UnitError("Cannot add/subtract: denominators mismatch")
-        elif (self.denominator is None) != (other.denominator is None):
-            raise UnitError("Cannot add/subtract: one has denominator, other does not")
-
-    def __add__(self: CompoundUnit, other: typing.Union[Unit, CompoundUnit]) -> CompoundUnit:
-        if isinstance(other, Unit):
-            if self.denominator is None:
-                return CompoundUnit(self.numerator + other)
-            else:
-                raise UnitError("Cannot add Unit directly to CompoundUnit with denominator")
-        self._check_compatibility(other)
-        if self.denominator:
-            new_numerator = self.numerator + other.numerator
-            return CompoundUnit(new_numerator, self.denominator)
-        else:
-            new_numerator = self.numerator + other.numerator
-            return CompoundUnit(new_numerator)
-
-    def __sub__(self: CompoundUnit, other: typing.Union[Unit, CompoundUnit]) -> CompoundUnit:
-        if isinstance(other, Unit):
-            if self.denominator is None:
-                return CompoundUnit(self.numerator - other)
-            else:
-                raise UnitError("Cannot subtract Unit directly from CompoundUnit with denominator")
-        self._check_compatibility(other)
-        if self.denominator:
-            new_numerator = self.numerator - other.numerator
-            return CompoundUnit(new_numerator, self.denominator)
-        else:
-            new_numerator = self.numerator - other.numerator
-            return CompoundUnit(new_numerator)
-
-    def __truediv__(self: CompoundUnit, other: typing.Union[Unit, CompoundUnit]) -> CompoundUnit:
-        if isinstance(other, Unit):
-            return CompoundUnit(self.numerator, self.denominator * other if self.denominator else other)
-        elif isinstance(other, CompoundUnit):
-            return CompoundUnit(self.numerator * other.denominator if other.denominator else self.numerator,
-                                self.denominator * other.numerator if self.denominator else other.numerator)
-        else:
-            raise TypeError("Can only divide by Unit or CompoundUnit")
-
-    def __mul__(self: CompoundUnit, other: typing.Union[Unit, CompoundUnit]) -> CompoundUnit:
-        if isinstance(other, Unit):
-            return CompoundUnit(self.numerator * other, self.denominator)
-        elif isinstance(other, CompoundUnit):
-            new_numerator = self.numerator * other.numerator
-            new_denominator: typing.Union["Unit", "CompoundUnit", None] = None
-            if self.denominator and other.denominator:
-                new_denominator = self.denominator * other.denominator
-            elif self.denominator:
-                new_denominator = self.denominator
-            elif other.denominator:
-                new_denominator = other.denominator
-            return CompoundUnit(new_numerator, new_denominator)
-        else:
-            raise TypeError("Can only multiply by Unit or CompoundUnit")
-
-    def __repr__(self: CompoundUnit) -> str:
-        if self.denominator is None:
-            return f"{self.numerator}"
-        num_str = f"({self.numerator})" if isinstance(self.numerator, CompoundUnit) else f"{self.numerator}"
-        den_str = f"({self.denominator})" if isinstance(self.denominator, CompoundUnit) else f"{self.denominator}"
-        return f"{num_str}/{den_str}"
-
-    def __eq__(self: CompoundUnit, other: object) -> bool:
-        if not isinstance(other, CompoundUnit):
-            raise UnitError("Units must be of the same type")
-        return (
-            self.numerator == other.numerator
-            and self.denominator == other.denominator
-        )
-
-# TODO:
-# Probably self.numerator & self.denominator are not correctly implemented
-# As of my understanding, they can also be CompoundUnit, and not only Unit
-# Which just breaks everything :joy:
+    def __init__(self, amount: typing.Union[int, float]):
+        amount = float(amount)
+        self.amount: float = amount
